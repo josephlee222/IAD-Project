6646af809c8017b01b959f71cacb678b077a16cf
Joseph222 <facebooklee52@gmail.com>
2021-03-02 14:51:12 +0800

    Minor change

--- admin_add.php
+++ admin_add.php
@@ -39,7 +39,7 @@
         <script src="./js/animations.js"></script>
         <script src="./js/admin.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Innovate Training - Admin (Delete Registant)</title>
+        <title>Innovate Training - Admin (Add Registant)</title>
     </head>
 
     <body>
